0f8f8cc201f3334d585ab36d4c1c503a9df06184
JoeyCorbett <134228957+JoeyCorbett@users.noreply.github.com>
2023-10-18 13:10:43 -0400

    Start of Project

---
+++ main.py
@@ -0,0 +1,23 @@
+
+def show_list():
+    todo_list = []
+
+def add_task():
+
+
+
+if __name__ == "__main__":
+    print("\nToDo App\n")
+
+    #list = show_list(list)
+    print("(No current tasks)\n")
+    print("1. Add Tasks\n2. Complete Tasks\n3. Delete Tasks")
+    choice = input("Enter Number:")
+    if choice == "1":
+        add = add_task()
+
+
+
+
+
+
