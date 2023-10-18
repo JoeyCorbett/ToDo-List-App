123f0d2d148b9fad111ca5608a71f1a8de2498b0
JoeyCorbett <134228957+JoeyCorbett@users.noreply.github.com>
2023-10-18 16:33:46 -0400

    Allow user to remove tasks from list

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import time
 
+
 def add_task(tasks):
     name = input("\nTask Name: ")
     tasks.append(name)
     return tasks
 
+
 def rm_task(tasks):
+    print()
     n = 1
     for task in tasks:
-        print(f"{n} {tasks}")
+        print(f"{n}. {task}")
         n += 1
-    name = int(input("Enter Task Number: "))
+    name = int(input("\nEnter Number to Delete: "))
+    print(tasks)
+    tasks.remove(tasks[1-name])
+    return tasks
 
 
 def main():
@@ -22,27 +28,25 @@ def main():
                 print(f"• {task}")
         else:
             print("• No Tasks")
-
         print("\n1. Add Tasks\n2. Complete Tasks\n3. Delete Tasks\n")
-        choice = input("Enter Number: ")
-        if choice == "1":
-            add_task(tasks)
-        #elif choice == 2:
+        while True:
+            choice = input("Enter Number: ")
+            if choice == "1":
+                add_task(tasks)
+                break
+                # elif choice == 2:
 
-        elif choice == "3":
-            if len(tasks) > 0:
-                rm_task(tasks)
+            elif choice == "3":
+                if len(tasks) > 0:
+                    rm_task(tasks)
+                    break
+                else:
+                    print("\nNo Tasks to Remove\n")
+                    time.sleep(.3)
             else:
-                print("\nNo Tasks to Remove")
-                time.sleep(1)
-
+                print("\nInvalid Input")
+                time.sleep(.5)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
