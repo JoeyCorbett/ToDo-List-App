b5cb49459be2554a9d6662390dfe713361e51d43
JoeyCorbett <134228957+JoeyCorbett@users.noreply.github.com>
2023-10-18 15:42:07 -0400

    Allow user to add tasks and display a new list

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
+import time
 
-def show_list():
-    todo_list = []
+def add_task(tasks):
+    name = input("\nTask Name: ")
+    tasks.append(name)
+    return tasks
 
-def add_task():
+def rm_task(tasks):
+    n = 1
+    for task in tasks:
+        print(f"{n} {tasks}")
+        n += 1
+    name = int(input("Enter Task Number: "))
+
+
+def main():
+    tasks = []
+    while True:
+        print("\nToDo App\n")
+        if len(tasks) > 0:
+            for task in tasks:
+                print(f"• {task}")
+        else:
+            print("• No Tasks")
+
+        print("\n1. Add Tasks\n2. Complete Tasks\n3. Delete Tasks\n")
+        choice = input("Enter Number: ")
+        if choice == "1":
+            add_task(tasks)
+        #elif choice == 2:
+
+        elif choice == "3":
+            if len(tasks) > 0:
+                rm_task(tasks)
+            else:
+                print("\nNo Tasks to Remove")
+                time.sleep(1)
 
 
 
 if __name__ == "__main__":
-    print("\nToDo App\n")
-
-    #list = show_list(list)
-    print("(No current tasks)\n")
-    print("1. Add Tasks\n2. Complete Tasks\n3. Delete Tasks")
-    choice = input("Enter Number:")
-    if choice == "1":
-        add = add_task()
+    main()
 
 
 
